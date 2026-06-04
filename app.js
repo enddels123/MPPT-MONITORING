67e900f5f38eb2b84f99b3458737330f7ec90ade
enddels123 <enddels@gmail.com>
2026-06-04 16:58:38 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const client = mqtt.connect(
-  "wss://broker.hivemq.com:8884/mqtt"
+const client =
+mqtt.connect(
+"wss://broker.hivemq.com:8884/mqtt"
 );
 
 const topic =
@@ -72,7 +73,6 @@ client.on("message",
   const data =
   JSON.parse(message.toString());
 
-  // PV
   document.getElementById("pv_v")
   .innerHTML =
   safe(data.pv_v).toFixed(1)+" V";
@@ -85,7 +85,10 @@ client.on("message",
   .innerHTML =
   safe(data.pv_p).toFixed(1)+" W";
 
-  // BATTERY
+  document.getElementById("pv_power_card")
+  .innerHTML =
+  safe(data.pv_p).toFixed(1)+" W";
+
   document.getElementById("bat_v")
   .innerHTML =
   safe(data.bat_v).toFixed(1)+" V";
@@ -98,22 +101,18 @@ client.on("message",
   .innerHTML =
   safe(data.soc).toFixed(0)+" %";
 
-  // LOAD
   document.getElementById("load_p")
   .innerHTML =
   safe(data.load_p).toFixed(1)+" W";
 
-  // CHARGE
   document.getElementById("charge_p")
   .innerHTML =
   safe(data.charge_p).toFixed(1)+" W";
 
-  // STATUS
   document.getElementById("status")
   .innerHTML =
-  data.status || "IDLE";
+  data.status;
 
-  // MPPT
   document.getElementById("mppt_v")
   .innerHTML =
   safe(data.mppt_v).toFixed(1)+" V";
@@ -126,33 +125,29 @@ client.on("message",
   .innerHTML =
   safe(data.mppt_p).toFixed(1)+" W";
 
-  // STATS
   document.getElementById("kwh_day")
   .innerHTML =
-  safe(data.kwh_day).toFixed(2)
-  +" kWh";
+  safe(data.kwh_day).toFixed(2)+" kWh";
+
+  document.getElementById("kwh_month")
+  .innerHTML =
+  safe(data.kwh_month).toFixed(2)+" kWh";
 
   document.getElementById("storage_prod")
   .innerHTML =
-  safe(data.storage_prod).toFixed(2)
-  +" kWh";
+  safe(data.storage_prod).toFixed(2)+" kWh";
 
   document.getElementById("charged")
   .innerHTML =
-  safe(data.charged).toFixed(2)
-  +" kWh";
+  safe(data.charged).toFixed(2)+" kWh";
 
   document.getElementById("load_cons")
   .innerHTML =
-  safe(data.load_cons).toFixed(2)
-  +" kWh";
+  safe(data.load_cons).toFixed(2)+" kWh";
 
   document.getElementById("mppt_eff")
   .innerHTML =
-  safe(data.mppt_eff).toFixed(1)
-  +" %";
-
-  /* ================= CHART ================= */
+  safe(data.mppt_eff).toFixed(1)+" %";
 
   const now =
   new Date()
