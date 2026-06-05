e5789f6df61da874421fb1dd5dbadf43d7498d07
enddels123 <enddels@gmail.com>
2026-06-05 13:50:17 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,30 +1,14 @@
 /* =========================================================
-APP.JS TERBARU FINAL
-ISOKUIKI SMART ENERGY
-========================================================= */
+   ISOKUIKI SMART ENERGY
+   FINAL FULL VERSION APP.JS
+   Firebase + Auto Reconnect + Animated Node
+   ========================================================= */
 
 /* =========================================================
-FIREBASE IMPORT
-========================================================= */
-
-import { initializeApp }
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
-import {
-getDatabase,
-ref,
-onValue,
-goOnline,
-goOffline
-}
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
-
-/* =========================================================
-FIREBASE CONFIG
-========================================================= */
+   FIREBASE CONFIG
+   ========================================================= */
 
 const firebaseConfig = {
-
 apiKey:"AIzaSyC7ctgLv34n6pwg9cQpcTN9qd77FbMGbOg",
 authDomain:"isokuiki-scada.firebaseapp.com",
 databaseURL:"https://isokuiki-scada-default-rtdb.asia-southeast1.firebasedatabase.app",
@@ -32,275 +16,403 @@ projectId:"isokuiki-scada",
 storageBucket:"isokuiki-scada.firebasestorage.app",
 messagingSenderId:"1078745557059",
 appId:"1:1078745557059:web:0f465f1a8a2ddf20dd8cf6"
+};
 
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.database();
+
+/* =========================================================
+   ELEMENT HELPER
+   ========================================================= */
+
+const $ = (id) => document.getElementById(id);
+
+/* =========================================================
+   ELEMENTS
+   ========================================================= */
+
+/* STATUS */
+const onlineStatus = $("onlineStatus");
+
+/* PV */
+const pvVoltage = $("pvVoltage");
+const pvCurrent = $("pvCurrent");
+const pvPower = $("pvPower");
+
+/* MPPT */
+const mpptCharge = $("mpptCharge");
+const mpptEff = $("mpptEff");
+
+/* BATTERY */
+const batVoltage = $("batVoltage");
+const batCurrent = $("batCurrent");
+const batSoc = $("batSoc");
+
+/* ENERGY */
+const energyDaily = $("energyDaily");
+const energyMonthly = $("energyMonthly");
+
+/* SCADA */
+const scadaPvPower = $("scadaPvPower");
+const scadaPvVolt = $("scadaPvVolt");
+const scadaPvAmp = $("scadaPvAmp");
+
+const scadaMpptPower = $("scadaMpptPower");
+const scadaMpptEff = $("scadaMpptEff");
+
+const scadaBatSoc = $("scadaBatSoc");
+const scadaBatVolt = $("scadaBatVolt");
+const scadaBatAmp = $("scadaBatAmp");
+
+const scadaLoadPower = $("scadaLoadPower");
+const scadaLoadVolt = $("scadaLoadVolt");
+const scadaLoadAmp = $("scadaLoadAmp");
+
+/* GAUGE */
+const pvGaugeValue = $("pvGaugeValue");
+const pvGaugeVolt = $("pvGaugeVolt");
+const pvGaugeAmp = $("pvGaugeAmp");
+
+const batteryGaugeValue = $("batteryGaugeValue");
+const effGaugeValue = $("effGaugeValue");
+
+/* NODE */
+const nodeMain = $("nodeMain");
+const nodeLeft = $("nodeLeft");
+const nodeRight = $("nodeRight");
+
+/* =========================================================
+   DEFAULT DATA
+   ========================================================= */
+
+let systemOnline = false;
+
+let dataCache = {
+  pv_voltage: 0,
+  pv_current: 0,
+  pv_power: 0,
+
+  mppt_charge: 0,
+  mppt_efficiency: 0,
+
+  battery_voltage: 0,
+  battery_current: 0,
+  battery_soc: 0,
+
+  load_voltage: 0,
+  load_current: 0,
+  load_power: 0,
+
+  energy_daily: 0,
+  energy_monthly: 0,
+
+  timestamp: 0
 };
 
 /* =========================================================
-INITIALIZE
-========================================================= */
+   FORMAT
+   ========================================================= */
 
-const app = initializeApp(firebaseConfig);
+function formatNumber(value, digit = 1) {
+  if (value === null || value === undefined || isNaN(value)) {
+    return "0";
+  }
 
-const db = getDatabase(app);
-
-/* =========================================================
-DATABASE PATH
-========================================================= */
-
-const monitoringRef = ref(db, "monitoring");
-
-/* =========================================================
-HELPER
-========================================================= */
-
-function setText(id, value){
-
-const el = document.getElementById(id);
-
-if(el){
-
-el.innerText = value;
-
-}
-
+  return Number(value).toFixed(digit);
 }
 
 /* =========================================================
-ONLINE STATUS
-========================================================= */
+   ONLINE OFFLINE STATUS
+   ========================================================= */
 
-function setOnlineStatus(isOnline){
+function setStatus(online) {
 
-const statusEl = document.getElementById("statusText");
+  if (online) {
 
-if(!statusEl) return;
+    onlineStatus.innerText = "ONLINE";
+    onlineStatus.style.color = "#7CFF00";
+    onlineStatus.style.textShadow = "0 0 10px #7CFF00";
 
-if(isOnline){
+  } else {
 
-statusEl.innerText = "ONLINE";
-
-statusEl.classList.remove("offline");
-
-statusEl.classList.add("online");
-
-}else{
-
-statusEl.innerText = "OFFLINE";
-
-statusEl.classList.remove("online");
-
-statusEl.classList.add("offline");
-
-}
-
+    onlineStatus.innerText = "OFFLINE";
+    onlineStatus.style.color = "#ff2b2b";
+    onlineStatus.style.textShadow = "0 0 10px #ff0000";
+  }
 }
 
 /* =========================================================
-FORMAT NUMBER
-========================================================= */
+   UPDATE UI
+   ========================================================= */
 
-function formatNumber(value){
+function updateUI(data) {
 
-return Number(value || 0).toFixed(1);
+  /* =========================
+     STATUS
+     ========================= */
 
+  const now = Date.now();
+  const last = data.timestamp || 0;
+
+  systemOnline = (now - last) < 20000;
+
+  setStatus(systemOnline);
+
+  /* =========================
+     PV INFO
+     ========================= */
+
+  pvVoltage.innerText = formatNumber(data.pv_voltage) + "V";
+  pvCurrent.innerText = formatNumber(data.pv_current) + "A";
+  pvPower.innerText = formatNumber(data.pv_power) + "W";
+
+  /* =========================
+     MPPT INFO
+     ========================= */
+
+  mpptCharge.innerText = formatNumber(data.mppt_charge) + "W";
+  mpptEff.innerText = formatNumber(data.mppt_efficiency) + "%";
+
+  /* =========================
+     BATTERY INFO
+     ========================= */
+
+  batVoltage.innerText = formatNumber(data.battery_voltage) + "V";
+  batCurrent.innerText = formatNumber(data.battery_current) + "A";
+  batSoc.innerText = formatNumber(data.battery_soc) + "%";
+
+  /* =========================
+     ENERGY
+     ========================= */
+
+  energyDaily.innerText = formatNumber(data.energy_daily, 2) + " kWh";
+  energyMonthly.innerText = formatNumber(data.energy_monthly, 2) + " kWh";
+
+  /* =========================
+     SCADA PV
+     ========================= */
+
+  scadaPvPower.innerText = formatNumber(data.pv_power) + "W";
+  scadaPvVolt.innerText = formatNumber(data.pv_voltage) + "V";
+  scadaPvAmp.innerText = formatNumber(data.pv_current) + "A";
+
+  /* =========================
+     SCADA MPPT
+     ========================= */
+
+  scadaMpptPower.innerText = formatNumber(data.mppt_charge) + "W";
+  scadaMpptEff.innerText = formatNumber(data.mppt_efficiency) + "%";
+
+  /* =========================
+     SCADA BATTERY
+     ========================= */
+
+  scadaBatSoc.innerText = formatNumber(data.battery_soc) + "%";
+  scadaBatVolt.innerText = formatNumber(data.battery_voltage) + "V";
+  scadaBatAmp.innerText = formatNumber(data.battery_current) + "A";
+
+  /* =========================
+     SCADA LOAD
+     ========================= */
+
+  scadaLoadPower.innerText = formatNumber(data.load_power) + "W";
+  scadaLoadVolt.innerText = formatNumber(data.load_voltage) + "V";
+  scadaLoadAmp.innerText = formatNumber(data.load_current) + "A";
+
+  /* =========================
+     GAUGE VALUE
+     ========================= */
+
+  pvGaugeValue.innerText = formatNumber(data.pv_power) + "W";
+  pvGaugeVolt.innerText = formatNumber(data.pv_voltage) + "V";
+  pvGaugeAmp.innerText = formatNumber(data.pv_current) + "A";
+
+  batteryGaugeValue.innerText = formatNumber(data.battery_soc) + "%";
+
+  effGaugeValue.innerText = formatNumber(data.mppt_efficiency) + "%";
+
+  /* =========================
+     NODE ANIMATION
+     ========================= */
+
+  animateNodes(data);
 }
 
 /* =========================================================
-UPDATE ALL UI
-========================================================= */
+   NODE ANIMATION
+   ========================================================= */
 
-function updateUI(data){
+let nodePosition = 0;
 
-/* =========================
-PV
-========================= */
+function animateNodes(data) {
 
-const pvVoltage = formatNumber(data.pvVoltage);
-const pvCurrent = formatNumber(data.pvCurrent);
-const pvPower   = formatNumber(data.pvPower);
+  const power = Number(data.pv_power || 0);
 
-/* =========================
-BATTERY
-========================= */
+  if (power <= 0) {
 
-const batVoltage = formatNumber(data.batteryVoltage);
-const batCurrent = formatNumber(data.batteryCurrent);
-const batterySOC = formatNumber(data.batterySOC);
+    nodeMain.style.opacity = 0.25;
+    nodeLeft.style.opacity = 0.25;
+    nodeRight.style.opacity = 0.25;
 
-/* =========================
-MPPT
-========================= */
+    return;
+  }
 
-const mpptEfficiency = formatNumber(data.mpptEfficiency);
-const mpptPower      = formatNumber(data.mpptPower || data.pvPower);
+  nodeMain.style.opacity = 1;
+  nodeLeft.style.opacity = 1;
+  nodeRight.style.opacity = 1;
 
-/* =========================
-LOAD
-========================= */
+  nodePosition += 1.5;
 
-const loadVoltage = formatNumber(data.loadVoltage || data.batteryVoltage);
+  if (nodePosition > 100) {
+    nodePosition = 0;
+  }
 
-const loadCurrent = formatNumber(data.loadCurrent || 0);
+  /* MAIN LINE */
+  nodeMain.style.offsetDistance = `${nodePosition}%`;
 
-const loadPower = formatNumber(data.loadPower || 0);
+  /* LEFT */
+  nodeLeft.style.offsetDistance = `${nodePosition}%`;
 
-/* =========================
-ENERGY
-========================= */
-
-const dailyEnergy =
-formatNumber(data.dailyEnergy || 0);
-
-const monthlyEnergy =
-formatNumber(data.monthlyEnergy || 0);
-
-/* =========================================================
-TOP DATA
-========================================================= */
-
-setText("pvVoltageText", pvVoltage + "V");
-setText("pvCurrentText", pvCurrent + "A");
-setText("pvPowerText", pvPower + "W");
-
-setText("batVoltageText", batVoltage + "V");
-setText("batCurrentText", batCurrent + "A");
-
-setText("socText", batterySOC + "%");
-
-setText("mpptPowerText", mpptPower + "W");
-setText("mpptEffText", mpptEfficiency + "%");
-
-setText("dailyEnergy", dailyEnergy + " kWh");
-setText("monthlyEnergy", monthlyEnergy + " kWh");
-
-/* =========================================================
-GAUGE
-========================================================= */
-
-setText("pvGaugePower", pvPower + "W");
-setText("pvGaugeVolt", pvVoltage + "V");
-setText("pvGaugeAmp", pvCurrent + "A");
-
-setText("socGauge", batterySOC + "%");
-
-setText("effGauge", mpptEfficiency + "%");
-
-/* =========================================================
-SCADA
-========================================================= */
-
-setText("pvScadaPower", pvPower + "W");
-
-setText(
-"pvScadaVA",
-pvVoltage + "V | " + pvCurrent + "A"
-);
-
-setText("mpptScadaPower", mpptPower + "W");
-
-setText(
-"mpptScadaEff",
-mpptEfficiency + "%"
-);
-
-setText("batScadaSoc", batterySOC + "%");
-
-setText(
-"batScadaVA",
-batVoltage + "V | " + batCurrent + "A"
-);
-
-setText("loadScadaPower", loadPower + "W");
-
-setText(
-"loadScadaVA",
-loadVoltage + "V | " + loadCurrent + "A"
-);
-
+  /* RIGHT */
+  nodeRight.style.offsetDistance = `${nodePosition}%`;
 }
 
 /* =========================================================
-FIREBASE REALTIME LISTENER
-========================================================= */
+   FIREBASE LISTENER
+   ========================================================= */
 
-onValue(
+function startFirebaseListener() {
 
-monitoringRef,
+  db.ref("smartenergy").on(
+    "value",
 
-(snapshot)=>{
+    (snapshot) => {
 
-setOnlineStatus(true);
+      const data = snapshot.val();
 
-const data = snapshot.val() || {};
+      if (!data) return;
 
-updateUI(data);
+      dataCache = {
 
-},
+        ...dataCache,
+        ...data
+      };
 
-(error)=>{
+      updateUI(dataCache);
+    },
 
-console.error("Firebase Error:", error);
+    (error) => {
 
-setOnlineStatus(false);
+      console.log("Firebase Error:", error);
 
+      setStatus(false);
+
+      reconnectFirebase();
+    }
+  );
 }
 
-);
+/* =========================================================
+   RECONNECT FIREBASE
+   ========================================================= */
+
+function reconnectFirebase() {
+
+  console.log("Reconnecting Firebase...");
+
+  setTimeout(() => {
+
+    try {
+
+      firebase.database().goOnline();
+
+      startFirebaseListener();
+
+    } catch (e) {
+
+      console.log(e);
+
+      reconnectFirebase();
+    }
+
+  }, 5000);
+}
 
 /* =========================================================
-AUTO RECONNECT
-========================================================= */
+   AUTO CHECK CONNECTION
+   ========================================================= */
 
-window.addEventListener("online", ()=>{
+setInterval(() => {
 
-console.log("Internet Connected");
+  const now = Date.now();
 
-goOnline(db);
+  if ((now - dataCache.timestamp) > 20000) {
 
-setOnlineStatus(true);
+    setStatus(false);
 
+  } else {
+
+    setStatus(true);
+  }
+
+}, 5000);
+
+/* =========================================================
+   AUTO RECONNECT INTERNET
+   ========================================================= */
+
+window.addEventListener("online", () => {
+
+  console.log("Internet Connected");
+
+  firebase.database().goOnline();
+
+  reconnectFirebase();
 });
 
-window.addEventListener("offline", ()=>{
+window.addEventListener("offline", () => {
 
-console.log("Internet Disconnected");
+  console.log("Internet Offline");
 
-goOffline(db);
-
-setOnlineStatus(false);
-
+  setStatus(false);
 });
 
 /* =========================================================
-KEEP CONNECTION ALIVE
-========================================================= */
+   START
+   ========================================================= */
 
-setInterval(()=>{
-
-if(navigator.onLine){
-
-goOnline(db);
-
-}
-
-}, 15000);
+startFirebaseListener();
 
 /* =========================================================
-PAGE VISIBILITY RECONNECT
-========================================================= */
+   OPTIONAL DEMO MODE
+   HAPUS BAGIAN INI JIKA SUDAH ADA DATA ESP32
+   ========================================================= */
 
-document.addEventListener("visibilitychange", ()=>{
+// setInterval(() => {
 
-if(document.visibilityState === "visible"){
+//   const fake = {
+//     pv_voltage: 21 + Math.random() * 3,
+//     pv_current: 4 + Math.random() * 2,
+//     pv_power: 80 + Math.random() * 40,
 
-goOnline(db);
+//     mppt_charge: 75 + Math.random() * 30,
+//     mppt_efficiency: 90 + Math.random() * 8,
 
-}
+//     battery_voltage: 12 + Math.random(),
+//     battery_current: 5 + Math.random(),
+//     battery_soc: 70 + Math.random() * 20,
 
-});
+//     load_voltage: 12,
+//     load_current: 2 + Math.random(),
+//     load_power: 20 + Math.random() * 20,
 
-/* =========================================================
-STARTUP
-========================================================= */
+//     energy_daily: 1 + Math.random(),
+//     energy_monthly: 25 + Math.random() * 5,
 
-console.log("ISOKUIKI SMART ENERGY READY");
+//     timestamp: Date.now()
+//   };
+
+//   updateUI(fake);
+
+// }, 1000);
